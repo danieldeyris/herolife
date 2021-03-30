4e72200e7e86c3a18ff5d66a69017cbb5f3aa4ab
danieldeyris <daniel.deyris@phidias.fr>
2021-03-31 00:36:19 +0200

    correction editions

--- herolife_specifique/__manifest__.py
+++ herolife_specifique/__manifest__.py
@@ -13,7 +13,7 @@
     'author': "Phidias",
     'website': "http://www.phidias.fr",
     'category': 'Uncategorized',
-    'version': '13.0.0.4',
+    'version': '13.0.0.5',
 
     # any module necessary for this one to work correctly
     'depends': [
